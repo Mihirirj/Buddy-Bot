bf9206b04b28e702df01dda254d50e77a8e16453
Mihiri Jayasena <122019143+Mihirirj@users.noreply.github.com>
2025-05-12 21:04:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-// src/main.jsx
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css'; // Imports global styles
-import App from './App.jsx'; // Imports your main App component
+import './index.css'; 
+import App from './App.jsx'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
